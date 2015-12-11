69866c3039e7e6c2a1e4fb5dc59fe3d9da4055b6
Anton Zheleznyak <antonanyaz@yandex.ru>
2015-12-11 15:24:28 +0200

    click

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     'use strict'
-    var m = 4;
-    var n = 5;
+    var m = 40;
+    var n = 50;
     for (var i = 0; i < m; i++) {
     	 $("#life").append('<tr id="tr-'+ i + '">');
     	for (var j = 0; j < n; j++) {
     		$("#life tr#tr-" + i).append('<td id="td-'+ j + '">');
     	};
     }
+    $("td").click(function () {
+    	$(this).addClass("life");
+    })
 });
